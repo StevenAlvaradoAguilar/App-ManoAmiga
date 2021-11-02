342d8f060ba1be3f7a369091aebc0cb9e3dc56c6
StevenAlvaradoAguilar <alvaradosteven6@gmail.com>
2021-11-01 19:30:52 -0600

    New features in the project

--- ManoAmiga/app/src/main/java/com/example/registeractivity/MainActivity.java
+++ ManoAmiga/app/src/main/java/com/example/registeractivity/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.registeractivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 import com.example.registeractivity.Session.SessionManager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +23,42 @@ public class MainActivity extends AppCompatActivity {
 
         sessionManager = new SessionManager(getApplicationContext());
         sessionManager.checkLogin();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+
+                        case R.id.navigation_generate_alert:
+                            selectedFragment = new DashboardFragment();
+                            break;
+                        case R.id.navigation_accept_alert:
+                            selectedFragment = new DashboardFragment();
+                            break;
+                        case R.id.navigation_contact:
+                            selectedFragment = new DashboardFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                    }
+        };
 }
---
+++ ManoAmiga/app/src/main/java/com/example/registeractivity/NotificationFragment.java
@@ -0,0 +1,20 @@
+package com.example.registeractivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.registeractivity.R;
+
+public class NotificationFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+}
