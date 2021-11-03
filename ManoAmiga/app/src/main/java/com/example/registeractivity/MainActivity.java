dd869095b58ea98a3308c3922566b4c855c475b0
StevenAlvaradoAguilar <alvaradosteven6@gmail.com>
2021-11-02 18:33:11 -0600

    New features in the project to improve

--- ManoAmiga/app/src/main/java/com/example/registeractivity/MainActivity.java
+++ ManoAmiga/app/src/main/java/com/example/registeractivity/MainActivity.java
@@ -6,8 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.registeractivity.Session.SessionManager;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -23,6 +29,33 @@ public class MainActivity extends AppCompatActivity {
 
         sessionManager = new SessionManager(getApplicationContext());
         sessionManager.checkLogin();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 10);
+
+
+        EditText editText = findViewById(R.id.editText);
+
+        findViewById(R.id.enterBtn)
+                .setOnClickListener(v -> {
+
+                    Intent intent = new Intent(this, ChatActivity.class);
+                    intent.putExtra("name", editText.getText().toString());
+                    startActivity(intent);
+
+                });
+
+        EditText editText1 = findViewById(R.id.editText);
+
+        findViewById(R.id.enterBtn)
+                .setOnClickListener(v -> {
+
+                    Intent intent = new Intent(this, ChatActivity.class);
+                    intent.putExtra("name", editText1.getText().toString());
+                    startActivity(intent);
+
+                });
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -46,18 +79,19 @@ public class MainActivity extends AppCompatActivity {
                             break;
 
                         case R.id.navigation_generate_alert:
-                            selectedFragment = new DashboardFragment();
+                            selectedFragment = new GenerateAlertFragment();
                             break;
+
                         case R.id.navigation_accept_alert:
-                            selectedFragment = new DashboardFragment();
+                            selectedFragment = new GenerateAlertFragment();
                             break;
+
                         case R.id.navigation_contact:
-                            selectedFragment = new DashboardFragment();
+                            selectedFragment = new GenerateAlertFragment();
                             break;
                     }
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                             selectedFragment).commit();
-
                     return true;
                     }
         };
